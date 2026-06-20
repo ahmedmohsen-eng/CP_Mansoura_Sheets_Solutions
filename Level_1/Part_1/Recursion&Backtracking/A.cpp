b48a6d61b88f3634992c8d27d8f2bbb3bc951d1b
Ahmed Mohsen <97984942+ahmedmohsen-eng@users.noreply.github.com>
2026-06-20 18:01:10 +0300

    Add recursive factorial function in A.cpp

---
+++ Level_1/Part_1/Recursion&Backtracking/A.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+int solve(int n){
+    if(n==0) return 1;
+    return n*solve(n-1);
+}
+signed main(){
+    int n;cin>>n;
+    cout<<solve(n);
+}
