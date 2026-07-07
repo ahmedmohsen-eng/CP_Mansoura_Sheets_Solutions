59e1d9380a8c4d8940bed11afb6ae70b493491d2
Ahmed Mohsen <97984942+ahmedmohsen-eng@users.noreply.github.com>
2026-07-07 14:11:04 +0300

    Add problem link to b.cpp

---
+++ Level_1/part_2/intro_to_graphs_and_dfs/b.cpp
@@ -0,0 +1,60 @@
+//problem link: https://vjudge.net/contest/690295#problem/B
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+#define int long long
+
+vector<vector<int>>adj;
+vector<int>vis;
+
+void dfs(int u){
+    vis[u]=1;
+    for(auto v:adj[u]){
+        if(!vis[v]){
+            dfs(v);
+        }
+    }
+}
+
+
+void solve(const int&tt){
+    // to be a tree, it must m=n-1 (edges=nodes-1)  
+    int n,m;cin>>n>>m;
+    adj.assign(n+1,{});
+    vis.assign(n+1,0);
+    
+    for(int i= 1 ; i<=m ; i++){
+        int u,v;cin>>u>>v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+    
+    int con_comps=0;
+    for(int i = 1 ; i < n; i++){
+        if(!vis[i]){
+            con_comps++;
+            dfs(i);
+        }
+    }
+    cout<<(n-1==m && con_comps==1?"YES":"NO");
+    
+}
+
+
+signed main(){
+    
+    #ifndef ONLINE_JUDGE
+    freopen("in.txt","r",stdin);
+    freopen("out.txt","w",stdout);
+    #endif
+    
+    int tt=1;
+    //cin>>tt;
+    while(tt--){
+        solve(tt);
+    }
+    
+    
+}
