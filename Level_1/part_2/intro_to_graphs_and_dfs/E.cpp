3b099dcc4b556b31b5c7bcb8d8472d8536e1455d
Ahmed Mohsen <97984942+ahmedmohsen-eng@users.noreply.github.com>
2026-07-07 19:46:40 +0300

    Add DFS solution for maze problem
    Implement depth-first search (DFS) for maze traversal.

---
+++ Level_1/part_2/intro_to_graphs_and_dfs/E.cpp
@@ -0,0 +1,88 @@
+//problem link : https://vjudge.net/contest/690295#problem/E
+
+//learnt and written from chatgpt
+//first time to deal with 2d dfs
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+#define int long long
+
+vector<vector<bool>>vis;
+
+int n,m;
+vector<string>maze;
+
+int dx[] = {1,-1,0,0};
+int dy[] = {0,0,1,-1};
+
+void dfs(int x, int y){
+    vis[x][y]=1;
+    
+    for (int k = 0; k < 4; k++) {
+        int nx = x + dx[k];
+        int ny = y + dy[k];
+
+        if (nx < 0 || nx >= n || ny < 0 || ny >= m)
+            continue;
+
+        if (maze[nx][ny] == '#')
+            continue;
+
+        if (vis[nx][ny])
+            continue;
+
+        dfs(nx, ny);
+    }
+    
+}
+
+
+
+void solve(const int&tt){
+    //dbg:
+    // cerr<<"reached";
+    
+    
+    cin>>n>>m;
+    maze.assign(n,{});
+    vis.assign(n, vector<bool>(m, false));
+    
+    for(int i= 0 ;i <n ;i++){
+        cin>>maze[i];
+    }
+    
+    
+    
+    int cnt=0;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (maze[i][j] == '.' && !vis[i][j]) {
+                dfs(i, j);
+                cnt++;
+            }
+        }
+    }
+    
+    cout<<cnt;
+    
+    
+}
+
+
+signed main(){
+    
+    #ifndef ONLINE_JUDGE
+    freopen("in.txt","r",stdin);
+    freopen("out.txt","w",stdout);
+    #endif
+    
+    int tt=1;
+    // cin>>tt;
+    while(tt--){
+        solve(tt);
+    }
+    
+    
+}
